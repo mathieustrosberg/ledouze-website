02ff0a37000224ff4da51e8c998f109c13e1718b
mathieustrosberg <mathieustrosberg@gmail.com>
2025-12-20 18:22:00 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -5,4 +6,13 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                mentions: resolve(__dirname, 'mentions-legales.html'),
+                conditions: resolve(__dirname, 'conditions-utilisation.html'),
+            },
+        },
+    },
 })
